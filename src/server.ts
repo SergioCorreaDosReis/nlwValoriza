34f2814b7d2201f4bd111ac4f7ce17d8a2dcec80
Sergio Reis <sergioc_reis@hotmail.com>
2021-06-22 15:52:40 -0300

    add User entitie and Create User Migration

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,7 @@
 import express from "express";
+import "reflect-metadata";
+import "./database";
 
 const app = express();
-
-app.get("/test", (request, response) => {
-  return response.send("Olá to no ar 👍");
-});
-
-app.post("/test-post", (request, response) => {
-  return response.send("Olá to no ar POST 👍");
-});
 
 app.listen(3000, () => console.log("Server is running!"));
